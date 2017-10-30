292b398160b357a9ac768c2351bb322fd4ebc4f3
Cody Lambert <codylambert@gmail.com>
2017-10-30 09:46:07 -0700

    Update AppVManage.ps1

--- PrePackaging/AppVManage/AppVManage.ps1
+++ PrePackaging/AppVManage/AppVManage.ps1
@@ -1,50 +1,46 @@
 #vSphere and Sequencer VM Configuration
-	$Sequencer = 'MachineName' #Computer name of Sequencer VM
-    $ODUser = "$Sequencer\ODSnap" #Local user admin account on Sequencer (Off Domain User)
-    $ODPFile = "$PSScriptRoot\..\..\AvalonSource\Pd.txt" #Encrypted password for $ODUser account
-    $ODKFile = "$PSScriptRoot\..\..\AvalonSource\Avalon.key" #Encryption key for password
-    $ODKey = Get-Content $ODKFile
-    $ODCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ODUser, (Get-Content $ODPFile | ConvertTo-SecureString -Key $ODKey)
+	$Sequencer = 'ComputerName' #Computer name of Sequencer VM
 	Write-Verbose "Please Enter Credentials for VSphere (Domain\User)"
-    $Global:DomCred = Get-Credential -Message 'VSphere Credentials'
-    $VIServerName = 'vSphereServerName' #vSphere VI Server Name
-    $SnapShotName = 'ODSnap4' #Sequencer VM SnapShot Name
+    	$DomCred = Get-Credential -Message 'VSphere Credentials'
+    	$VIServerName = 'VIServer.domain.com' #vSphere VI Server Name
+	$SnapShotName = 'SnapshotName' #Sequencer VM SnapShot Name
 
 ##DO NOT MODIFY BELOW THIS LINE##	
 
 #Avalon Configuration
 	$VerbosePreference = "continue"
-    $AppSourceName = Split-Path -Path $PSScriptRoot -Leaf
+    	$AppSourceName = Split-Path -Path $PSScriptRoot -Leaf
 	$Source = "$PSScriptRoot\..\..\PrePackaging\$AppSourceName"
 	$Repo = "$PSScriptRoot\..\..\CompletedPackages"
-	$SDModuleSource = "$PSScriptRoot\..\..\Modules\SDModules.ps1" #Location of Avalon Custom Functions Script
+	$SDModuleSource = "$PSScriptRoot\..\..\Modules\SDModules.psm1" #Location of Avalon Custom Functions Script
 
 #region PowerCLI
 	Write-Verbose "Starting VM"
 	Add-PSSnapin -Name VMware.VimAutomation.Core
-    $ErrorActionPreference = 'SilentlyContinue'
-    Connect-VIServer -Server $VIServerName -Protocol https -Credential $DomCred
-    Get-Snapshot -VM $Sequencer -Name $SnapShotName | Set-VM -VM $Sequencer -Confirm:$false
+	$ErrorActionPreference = 'SilentlyContinue'
+    	Connect-VIServer -Server $VIServerName -Protocol https -Credential $DomCred
+    	Get-Snapshot -VM $Sequencer -Name $SnapShotName | Set-VM -VM $Sequencer -Confirm:$false
 	Write-Verbose "Waiting for VM IP Address"
-    Start-VM -VM $Sequencer
-    Do {
-        Start-Sleep -s 30
-        $SeqIP = Get-VM -Name $Sequencer | Select @{N="IPAddress";E={@($_.guest.IPAddress[0])}} | Select -Expand IPAddress
-    } While ($SeqIP.Length -eq 0)
+    	Start-VM -VM $Sequencer
+    		Do {
+        	Start-Sleep -s 30
+        	$VMInfo = Get-VMGuest $Sequencer
+        	$SeqIP = $VMinfo.IPAddress
+    		} While ($SeqIP.Length -eq 0)
 	Write-Verbose "Completed Starting VM"
 #endregion
 
 #Copy install files to Sequencer (C:\Avalon)
 	Write-Verbose "Starting File Copy to VM"
-    $LocalRoot = "\\$SeqIP\C$"
+    	$LocalRoot = "\\$SeqIP\C$"
 	$LocalDest  = "$LocalRoot\Avalon"
-    If (Test-Path $LocalRoot ) {
+    	If (Test-Path $LocalRoot ) {
         Net Use /delete $LocalRoot
 	}
 	If (Test-Path M:) {
 		Remove-PSDrive M -Force
 	}
-	New-PSDrive -Name M -PSProvider FileSystem -Root $LocalRoot -Credential $ODCred -Persist
+	New-PSDrive -Name M -PSProvider FileSystem -Root $LocalRoot -Credential $DomCred -Persist
     $ProcessFolder = "$LocalDest\Process"
 	New-Item -ItemType directory -Path $ProcessFolder -Force
 	Copy-Item $SDModuleSource "$ProcessFolder\SDModules.psm1" -Force
@@ -62,10 +58,8 @@
 	
 #Copy Package to Repository
 	Write-Verbose "Attempting to Copy AppV Package to Repository"
-    Remove-PSDrive M -Force
-	New-PSDrive -Name M -PSProvider FileSystem -Root $LocalRoot -Credential $ODCred -Persist
 	Copy-Item $LocalDest\* $Repo -Recurse -Exclude Process
-    Remove-PSDrive M -Force
+    	Remove-PSDrive M -Force
 	Write-Verbose "Completed Attempt to Copy AppV Package to Repository"
 
 #Reset VM to SnapShot
