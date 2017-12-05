8dfba00a30d04e183966ab36ac0587da90249992
Cody Lambert <codylambert@gmail.com>
2017-12-05 12:50:14 -0700

    Update process.ps1

--- Templates/File Copy Installation with PVAD/process.ps1
+++ Templates/File Copy Installation with PVAD/process.ps1
@@ -1,4 +1,13 @@
-$AppName = 'Application Name' #The application name, as appears on Start Menu
+#Logging Setup
+  $VerbosePreference = "Continue"
+  $AppName = 'Application Name' #The application name, as appears on Start Menu
+  $ErrorActionPreference="SilentlyContinue"
+	Stop-Transcript | out-null
+	$ErrorActionPreference = "Continue"
+	New-Item -ItemType directory -path "C:\Avalon\logging" -Force
+	$DStamp = Get-Date -Format dMMMy_ms
+	Start-Transcript -path "C:\Avalon\logging\${AppName}_Process_${DStamp}.txt"
+
 $LocalDest = 'C:\Avalon'
 $ProcessFolder = "$LocalDest\Process"
 Import-Module "$ProcessFolder\SDModules.psm1"
@@ -17,3 +26,5 @@ $Shortcut.TargetPath = $AppLocation
 $Shortcut.IconLocation = "$InstallPath\AppName.exe, 0" #Update with the icon name
 $Shortcut.WorkingDirectory ="$InstallPath"
 $Shortcut.Save()
+
+Stop-Transcript
